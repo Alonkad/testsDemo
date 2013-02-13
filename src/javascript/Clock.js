dc9b369a2b2e1f9c1c821a30a4c71cb6d14bd10a
Omry Nachman <omri@wix.com>
2013-02-13 15:48:51 +0200

    got 2 tests to pass

--- src/javascript/Clock.js
+++ src/javascript/Clock.js
@@ -1,10 +1,35 @@
 var Clock = (function () {
-    'use strict';
-    var ClockClass = function (eventListener) {
-
+    var ClockClass = function () {
+        this.alarms = [];
+        $('#ok').on('click', this.addAlarm.bind(this));
     };
 
     ClockClass.prototype = {
+        setAlarmTimeout: function (alarm) {
+            var timeToAlert = alarm.time.getTime() - Date.now();
+            setTimeout(function () {
+                alert(alarm.message);
+            }, timeToAlert)
+        },
+
+        addAlarm: function () {
+            var alarmTime = $('#time')[0].valueAsDate;
+            var now = new Date();
+            alarmTime.setDate(now.getDate());
+            alarmTime.setMonth(now.getMonth());
+            alarmTime.setYear(now.getYear());
+
+            var alarm = {
+                time: alarmTime,
+                message: $('#message').val()
+            };
+            //this.alarms.push(alarm);
+            this.setAlarmTimeout(alarm);
+            this.showAlarm(alarm);
+        },
+        showAlarm: function(alarm){
+            $('#alarms').append('<li>' + alarm.message + '</li>');
+        }
 
     }
     return ClockClass;
--- test/specs/Clock.spec.js
+++ test/specs/Clock.spec.js
@@ -1,33 +1,68 @@
 describe("Clock integration", function () {
-    beforeEach(function(){
-        this.view = document.getElementById('view').contentDocument;
+    'use strict';
+    beforeEach(function () {
+        var viewFrame = document.getElementById('view');
+        this.view = viewFrame.contentDocument;
+        this.window = viewFrame.contentWindow;
+
+        spyOn(this.window, 'alert');
+
+        this.userAddAlarm = function (alarmMessage, alarmTime) {
+            this.addAlarmView.message.value = alarmMessage;
+            this.addAlarmView.time.valueAsDate = alarmTime;
+
+            this.addAlarmView.ok.click();
+        }
+    });
+    afterEach(function () {
+        this.window.$('input[type!=button]').val('');
+        this.window.$('#alarms').empty();
+    });
+    beforeEach(function () {
+        this.addMatchers({
+            toIncludeAlarm: function (message, time) {
+                this.message = function () {
+                    return 'no item with the message: ' + message;// + ' and time ' + time;
+                }
+                var expectedItem = '<li>' + message + '</li>';
+
+                return this.actual.innerHTML.indexOf(expectedItem) >= 0;
+            }
+        })
     });
 
+
     describe('when the user adds a new alarm', function () {
-        beforeEach(function(){
-           this.addAlarmView = {
-               message: this.view.getElementById('message'),
-               ok: this.view.getElementById('ok'),
-               time: this.view.getElementById('time')
-           };
+        beforeEach(function () {
+            this.addAlarmView = {
+                message: this.view.getElementById('message'),
+                ok: this.view.getElementById('ok'),
+                time: this.view.getElementById('time')
+            };
             this.alarmsList = this.view.getElementById('alarms');
         });
 
         it('should be added to the alarms list', function () {
             var alarmMessage = 'a new alarm message';
-            var alarmDate = new Date();
-            this.addAlarmView.message.value = alarmMessage;
-            this.addAlarmView.time.valueAsDate = alarmDate;
-
-            this.addAlarmView.ok.click();
-
-            expect(this.alarmsList.innerHTML).toEqual('')
-
+            var alarmTime = new Date();
+            this.userAddAlarm(alarmMessage, alarmTime);
+            expect(this.alarmsList).toIncludeAlarm(alarmMessage, alarmTime);
         });
 
         describe('when the alarm time arrives', function () {
             it('should alert the alarm message', function () {
-                this.fail('test not implemented');
+                var self = this;
+                var alarmMessage = 'a new alarm message';
+                var alarmTime = new Date(Date.now() + 10);
+
+                this.userAddAlarm(alarmMessage, alarmTime);
+
+                waitsFor(function(){
+                    if (self.window.alert.callCount > 0) {
+                        expect(self.window.alert).toHaveBeenCalledWith(alarmMessage);
+                        return true;
+                    }
+                }, 'alert to be called', 20);
             });
 
             it('should not alert for removed alarms', function () {
